ae6e28ec1133ebddf5eb5749df8292e81d8f308d
Asa Grace <asag182@gmail.com>
2021-06-21 14:01:11 -0600

    add question to input name

--- src/page-template.js
+++ src/page-template.js
@@ -17,10 +17,4 @@ const generatePage = (name, github) => {
     `;
 };
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
 module.exports = generatePage;
